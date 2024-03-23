002147a6de2e16542dee0864f8a7591e2682539a
Henry Reyes <reyesh2@uci.edu>
2024-03-23 11:58:22 -0700

    goroutine to handle input and messages from server

--- client/control/control.go
+++ client/control/control.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"encoding/binary"
 	"fmt"
 	"io"
@@ -8,6 +9,7 @@ import (
 	"os"
 	"station/protocol"
 	"strconv"
+	"strings"
 )
 
 func main() {
@@ -38,30 +40,72 @@ func main() {
 		os.Exit(1)
 	}
 
-	for {
-		var messageType uint8
-		err := binary.Read(conn, binary.BigEndian, &messageType)
-		if err != nil {
-			if err != io.EOF {
-				fmt.Printf("Error reading message type: %v\n", err)
-			}
-			return
-		}
-
-		switch messageType {
-		case protocol.WelcomeReplyType:
-
-			var numStations uint16
-			err := binary.Read(conn, binary.BigEndian, &numStations)
+	go func() {
+		for {
+			var messageType uint8
+			err := binary.Read(conn, binary.BigEndian, &messageType)
 			if err != nil {
-				fmt.Printf("Error reading Welcome message data: %v\n", err)
-				break
+				if err != io.EOF {
+					fmt.Printf("Error reading message type: %v\n", err)
+				}
+				return
 			}
-			fmt.Printf("Received Welcome message with %d stations\n", numStations)
-		default:
-			fmt.Printf("Unknown message type received: %d\n", messageType)
 
+			switch messageType {
+			case protocol.WelcomeReplyType:
+				var numStations uint16
+				err := binary.Read(conn, binary.BigEndian, &numStations)
+				if err != nil {
+					fmt.Printf("Error reading Welcome message data: %v\n", err)
+					break
+				}
+				fmt.Printf("Welcome to the radio. There are %d stations\n", numStations)
+			default:
+				fmt.Printf("Unknown message type received: %d\n", messageType)
+
+			}
 		}
+
+	}()
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		input := scanner.Text()
+		args := strings.Split(input, " ")
+
+		if len(args) == 0 {
+			continue
+		}
+
+		switch args[0] {
+
+		case "set":
+			if len(args) < 3 || args[1] != "station" {
+				fmt.Println("Invalid command. Usage: 'set station <index>'")
+				continue
+			}
+
+			stationIndex, err := strconv.Atoi(args[2])
+			if err != nil {
+				fmt.Printf("Invalid station index: %v\n", err)
+				continue
+			}
+			fmt.Printf("CLIENT INPUT:[%d]", stationIndex)
+			setStationMsg := protocol.SetStationMessage(int32(stationIndex))
+			_, err = conn.Write(setStationMsg)
+			if err != nil {
+				fmt.Printf("Failed to send SetStation message: %v\n", err)
+				continue
+			}
+
+		default:
+			fmt.Println("Unknown command:", args[0])
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
 	}
 
 }
