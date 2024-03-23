cd3cd56ac8b2aa59597630cdbc13014471359926
Henry Reyes <reyesh2@uci.edu>
2024-03-22 23:31:05 -0700

    station protocol

--- protocol/protocol.go
+++ protocol/protocol.go
@@ -3,11 +3,14 @@ package protocol
 import (
 	"bytes"
 	"encoding/binary"
+	"errors"
 )
 
 const (
-	HelloCommandType = 0
-	WelcomeReplyType = 2
+	HelloCommandType   = 0
+	WelcomeReplyType   = 2
+	ChangeStationType  = 3
+	StationChangedType = 4
 )
 
 func HelloMessage(udpPort uint16) []byte {
@@ -35,5 +38,25 @@ func WelcomeMessage(numStations uint16) []byte {
 
 	binary.Write(buf, binary.BigEndian, uint8(WelcomeReplyType))
 	binary.Write(buf, binary.BigEndian, numStations)
+	return buf.Bytes()
+}
+
+func ParseStationChangedMessage(data []byte) (stationIndex int32, err error) {
+	buf := bytes.NewBuffer(data)
+	commandType, err := buf.ReadByte()
+	if err != nil {
+		return 0, err
+	}
+	if commandType != StationChangedType {
+		return 0, errors.New("incorrect message type for StationChanged")
+	}
+	err = binary.Read(buf, binary.BigEndian, &stationIndex)
+	return stationIndex, err
+}
+
+func StationChangedMessage(stationIndex int32) []byte {
+	buf := new(bytes.Buffer)
+	binary.Write(buf, binary.BigEndian, uint8(StationChangedType))
+	binary.Write(buf, binary.BigEndian, stationIndex)
 	return buf.Bytes()
 }
