5955aaabba975c3406056ae93572b8859b3b7610
Henry Reyes <reyesh2@uci.edu>
2024-03-26 09:45:40 -0700

    prefix length protocol

--- protocol/protocol.go
+++ protocol/protocol.go
@@ -4,7 +4,15 @@ import (
 	"bytes"
 	"encoding/binary"
 	"fmt"
+	"io"
+	"net"
 )
+
+type Metadata struct {
+	Title  string
+	Artist string
+	Album  string
+}
 
 const (
 	HelloCommandType   = 0
@@ -12,6 +20,7 @@ const (
 	SetStationType     = 3
 	StationChangedType = 4
 	InvalidCommandType = 5
+	MetadataType       = 6
 )
 
 func HelloMessage(udpPort uint16) []byte {
@@ -56,9 +65,66 @@ func SetStationMessage(stationIndex int32) []byte {
 	return buf.Bytes()
 }
 
+func MetadataMessage(title, artist, album string) []byte {
+	buf := new(bytes.Buffer)
+
+	// First, write the message type
+	binary.Write(buf, binary.BigEndian, uint8(MetadataType))
+
+	writeLengthPrefixedString(buf, title)
+	writeLengthPrefixedString(buf, artist)
+	writeLengthPrefixedString(buf, album)
+
+	return buf.Bytes()
+}
+
+func writeLengthPrefixedString(buf *bytes.Buffer, s string) {
+	// Convert the string to a byte slice
+	strBytes := []byte(s)
+	// Write the length of the string as a uint16
+	binary.Write(buf, binary.BigEndian, uint16(len(strBytes)))
+	// Write the string bytes
+	buf.Write(strBytes)
+}
+
 func InvalidCommandMessage(reason string) []byte {
 	buf := new(bytes.Buffer)
 	binary.Write(buf, binary.BigEndian, uint8(InvalidCommandType))
 	binary.Write(buf, binary.BigEndian, []byte(reason))
 	return buf.Bytes()
+}
+
+func ParseMetadataMessage(conn net.Conn) (Metadata, error) {
+	var metadata Metadata
+	// Read the title length and title
+	var titleLength uint16
+	if err := binary.Read(conn, binary.BigEndian, &titleLength); err != nil {
+		return metadata, err
+	}
+	title := make([]byte, titleLength)
+	if _, err := io.ReadFull(conn, title); err != nil {
+		return metadata, err
+	}
+	metadata.Title = string(title)
+
+	var artistLength uint16
+	if err := binary.Read(conn, binary.BigEndian, &artistLength); err != nil {
+		return metadata, err
+	}
+	artist := make([]byte, titleLength)
+	if _, err := io.ReadFull(conn, artist); err != nil {
+		return metadata, err
+	}
+	metadata.Artist = string(artist)
+
+	var albumLength uint16
+	if err := binary.Read(conn, binary.BigEndian, &albumLength); err != nil {
+		return metadata, err
+	}
+	album := make([]byte, albumLength)
+	if _, err := io.ReadFull(conn, artist); err != nil {
+		return metadata, err
+	}
+	metadata.Album = string(album)
+	return metadata, nil
 }
